1b4a1d63bd023b6179ac978dfafbceb93c2e8d18
odaniel1 <owendnl@gmail.com>
2023-08-05 15:37:55 +0100

    introduce utils and tests

--- R/api_functions.R
+++ R/api_functions.R
@@ -1,18 +1,17 @@
 #' @title Get electricity consumption data from n3rgy API
 #'
 #' @description Returns energy consumption data available from n3rgy.
-#' @param mac The MAC number (also known as GUID/EUI) of the smartmeter of interest.
-#' @param from The earliest date you want data from (n3rgy stores data for 13 months).
-#' @param to The latest date that you want data until.
+#' @param mac The MAC number (also known as GUID/EUI) of the smart meter of interest.
+#' @param from (Optional) A date, or string in %Y-%m-%d format representing the earliest date you want data from (n3rgy stores data for 13 months).
+#' @param to (Optional) A date, or string in %Y-%m-%d format representing the latest date that you want data until.
 #' @export
-n3rgy_consumption <- function(mac, from, to){
+n3rgy_consumption <- function(mac, from = NULL, to = NULL){
 
-  mac <- gsub("-","",mac)
+  mac <- clean_mac(mac)
+  url <- create_url("consumption", from, to)
+  resp <- httr::GET(url, httr::add_headers(Authorization=mac))
 
-  url <- "https://consumer-api.data.n3rgy.com/electricity/consumption/1/"
-  qry <- paste0(url, '?start=',from, '&end=', to)
-
-  resp <- httr::GET(qry, httr::add_headers(Authorization=mac))
+  check_api_response(resp)
 
   cont <- jsonlite::fromJSON(
     httr::content(resp,"text",encoding = "UTF-8"),
@@ -30,23 +29,22 @@ n3rgy_consumption <- function(mac, from, to){
 #' @title Get electricity tariff data from n3rgy API
 #'
 #' @description Returns electricity tariff data available from n3rgy.
-#' @param mac The MAC number (also known as GUID/EUI) of the smartmeter of interest.
-#' @param from The earliest date you want data from (n3rgy stores data for 13 months).
-#' @param to The latest date that you want data until.
+#' @param mac The MAC number (also known as GUID/EUI) of the smart meter of interest.
+#' @param from (Optional) A date, or string in %Y-%m-%d format representing the earliest date you want data from (n3rgy stores data for 13 months).
+#' @param to (Optional) A date, or string in %Y-%m-%d format representing the latest date that you want data until.
 #' @export
-n3rgy_tariff <- function(mac, from, to){
+n3rgy_tariff <- function(mac, from = NULL, to = NULL){
 
-  mac <- gsub("-","",mac)
-
-  url <- "https://consumer-api.data.n3rgy.com/electricity/tariff/1/"
-  qry <- paste0(url, '?start=',from, '&end=', to)
-
-  resp <- httr::GET(qry, httr::add_headers(Authorization=mac))
+  mac <- clean_mac(mac)
+  url <- create_url("tariff", from, to)
+  resp <- httr::GET(url, httr::add_headers(Authorization=mac))
 
   cont <- jsonlite::fromJSON(
     httr::content(resp,"text",encoding = "UTF-8"),
     simplifyVector = FALSE
   )
+
+  check_api_response(resp)
 
   info <- cont$values[[1]]$additionalInformation
   message(info)
